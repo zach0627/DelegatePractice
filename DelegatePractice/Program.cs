e0683ba227383a3fe9a7d08daf3a47eb7d2bfb4b
zach_liu <zach_liu@mayohr.com>
2024-08-13 23:21:04 +0800

    add func solution to delegate

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -1,15 +1,24 @@
 ﻿
+using System.Text;
+
 class Program
 {
     public delegate void MyDelegate(string message);
     static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
         MyDelegate myDelegate = new MyDelegate(PrintMessage);
         myDelegate += WriteMessage;
         Action<string> actionString = message => Console.WriteLine($"Hello from Action {message}");
+        //Action<string> 轉換成 MyDelegate
         myDelegate += new MyDelegate(actionString);
+        Func<string, string> functionString = delegate (string message) { return $"Hello from Func {message}"; };
+        //Lambda 包裝一下
+        myDelegate += (message) => Console.WriteLine(functionString(message));
         myDelegate += message => Console.WriteLine($"Hello from Lambda {message}");
 
+        Console.WriteLine("請先輸入文字");
         while (true)
         {
             string msg = Console.ReadLine();
--- DelegatePractice/obj/Debug/net8.0/DelegatePractice.AssemblyInfo.cs
+++ DelegatePractice/obj/Debug/net8.0/DelegatePractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DelegatePractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c547873684dfd35196eaafe1a26b6262965ba659")]
 [assembly: System.Reflection.AssemblyProductAttribute("DelegatePractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DelegatePractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
