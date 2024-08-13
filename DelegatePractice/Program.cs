c547873684dfd35196eaafe1a26b6262965ba659
zach_liu <zach_liu@mayohr.com>
2024-08-13 22:52:26 +0800

    upload new project

---
+++ DelegatePractice/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    public delegate void MyDelegate(string message);
+    static void Main(string[] args)
+    {
+        MyDelegate myDelegate = new MyDelegate(PrintMessage);
+        myDelegate += WriteMessage;
+        Action<string> actionString = message => Console.WriteLine($"Hello from Action {message}");
+        myDelegate += new MyDelegate(actionString);
+        myDelegate += message => Console.WriteLine($"Hello from Lambda {message}");
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            myDelegate.Invoke(msg);
+        }
+    }
+
+    static void WriteMessage(string message)
+    {
+        Console.WriteLine($"Hello~{message}");
+    }
+
+    static void PrintMessage(string message)
+    {
+        Console.WriteLine($"Print~~{message}");
+    }
+
+}
+
